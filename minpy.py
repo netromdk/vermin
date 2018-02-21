201f1a6196260ae15d6504784385f7e2b87a4694
Morten Kristensen <msk@nullpointer.dk>
2018-02-21 23:07:49 +0100

    Parse source using ast module

--- minpy.py
+++ minpy.py
@@ -4,51 +4,30 @@
 # version to determine what minimum version they need!
 
 import sys
-import parser
-from symbol import sym_name
-from token import tok_name
+import ast
 from pprint import pprint
 
 # Module requirements: name -> min version per major
 MOD_REQS = {"argparse": (2.7, 3.2)}
 
 def parse_source(source):
-  """Parse python source into a tree of statements and expressions."""
-  return parser.suite(source).totuple()
+  """Parse python source into an AST."""
+  return ast.parse(source)
 
 def parse_source_file(path):
-  """Parse python source file into a tree of statements and expressions."""
+  """Parse python source file into an AST."""
   with open(path, "r") as fp:
     return parse_source(fp.read())
 
-def parse_expression(expr):
-  """Parse python expression into a tree of statements and expression."""
-  return parser.expr(expr).totuple()
-
-def symbol_or_token_name(num):
-  if num in sym_name:
-    return sym_name[num]
-  if num in tok_name:
-    return tok_name[num]
-  return None
-
-def print_tree(tree, indent=0, parent=None):
-  if isinstance(tree, tuple):
-    for elm in tree:
-      indent_str = "  " * indent
-      if isinstance(elm, int):
-        print(indent_str, symbol_or_token_name(elm), "({})".format(elm))
-      elif isinstance(elm, tuple):
-        print_tree(elm, indent=indent+1, parent=tree)
-      else:
-        print(indent_str, elm, type(elm))
+class SourceVisitor(ast.NodeVisitor):
+  pass
 
 # TODO: Find modules by import: import X and from X import Y..
 # TODO: For each found module check version requirements.
-def find_modules(tree):
+def find_modules(node):
   pass
 
-# TODO: Maybe impl. class with visitor pattern that keeps track of the sub tree it is visiting, like
+# TODO: Maybe impl. class with visitor pattern that keeps track of the sub node it is visiting, like
 # import X, from X import Y, class, function etc.
 
 # TODO: Detect version from language use, like "print expr" (2) vs "print(expr)" (3)
@@ -56,10 +35,10 @@ def find_modules(tree):
 if __name__ == "__main__":
   try:
     if len(sys.argv) == 2:
-      tree = parse_source_file(sys.argv[1])
+      node = parse_source_file(sys.argv[1])
     else:
-      tree = parse_source("import argparse")
-      #tree = parse_source("from argparse import ArgumentParser")
-    print_tree(tree)
+      #node = parse_source("import argparse")
+      node = parse_source("from argparse import ArgumentParser")
+      pprint(ast.dump(node))
   except SyntaxError as err:
     print("Could not parse input: {}".format(err))
