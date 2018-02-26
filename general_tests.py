7e81455615348300b3578a02c957fb1e76a93ad4
Morten Kristensen <msk@nullpointer.dk>
2018-02-26 22:53:09 +0100

    print v2/v3 correctly tested for

--- general_tests.py
+++ general_tests.py
@@ -1,4 +1,4 @@
-from testutils import MinpyTest
+from testutils import MinpyTest, current_version
 from minpy import SourceVisitor, parse_source, parse_detect_source
 
 def visit(source):
@@ -8,13 +8,25 @@ def visit(source):
 
 class MinpyGeneralTests(MinpyTest):
   def test_printv2(self):
-    (node, mins) = parse_detect_source("print 'hello'")
-    self.assertEqual(node, None)
-    self.assertOnlyIn(2.0, mins)
+    source = "print 'hello'"
+    (node, mins) = parse_detect_source(source)
+    v = current_version()
+    if v >= 3.4:
+      self.assertEqual(node, None)
+      self.assertOnlyIn(2.0, mins)
+    elif v >= 3.0 and v < 3.4:
+      self.assertEqual(node, None)
+      self.assertEqual(mins, [None, None])
+    else:  # < 3.0
+      visitor = visit(source)
+      self.assertTrue(visitor.printv2())
 
   def test_printv3(self):
     visitor = visit("print('hello')")
-    self.assertTrue(visitor.printv3())
+    if current_version() >= 3.0:
+      self.assertTrue(visitor.printv3())
+    else:
+      self.assertTrue(visitor.printv2())
 
   def test_format(self):
     visitor = visit("print('{}'.format(42))")
