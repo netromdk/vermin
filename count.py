0465b065e1d2d58daf0e8c33edd756d26605af49
Morten Kristensen <msk@nullpointer.dk>
2018-03-05 19:40:52 +0100

    Converted MOD_MEM_REQS to a multidict to support members with same name

--- count.py
+++ count.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from minpy import MOD_REQS, MOD_MEM_REQS, KWARGS_REQS, STRFTIME_REQS
-(mods, mems, kwargs, dirs) =\
-  (len(MOD_REQS), len(MOD_MEM_REQS), len(KWARGS_REQS), len(STRFTIME_REQS))
+(mods, kwargs, dirs) = (len(MOD_REQS), len(KWARGS_REQS), len(STRFTIME_REQS))
+mems = 0
+for v in MOD_MEM_REQS.values():
+  mems += len(v)
 print("{:>4} modules\n{:>4} members\n{:>4} kwargs\n{:>4} strftime directives\n{:>4} total".
       format(mods, mems, kwargs, dirs, mods + mems + kwargs + dirs))
