cc81835f9767ff2c8b9787923c230a336d583b2b
Morten Kristensen <msk@nullpointer.dk>
2018-03-05 19:51:20 +0100

    When SyntaxError text is None treat as empty string

--- minpy.py
+++ minpy.py
@@ -538,13 +538,14 @@ def parse_detect_source(source, path=None):
   try:
     return (parse_source(source, path=path), [])
   except SyntaxError as err:
+    text = err.text.strip() if err.text is not None else ""
     # `print expr` is a Python 2 construct, in v3 it's `print(expr)`.
     # NOTE: This is only triggered when running a python 3 on v2 code!
     if err.msg.lower().find("missing parentheses in call to 'print'") != -1:
-      vvprint("`{}` requires 2.0".format(err.text.strip()))
+      vvprint("`{}` requires 2.0".format(text))
       return (None, [2.0, None])
     else:
-      vvprint("{}: {}".format(err, err.text.strip()))
+      vvprint("{}: {}".format(err, text))
   return (None, [0, 0])
 
 def detect_min_versions_source(source, path=None):
