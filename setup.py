dee854407b38a655c0079b1d6b6616ffb596e3ae
Morten Kristensen <msk@nullpointer.dk>
2018-03-06 21:13:32 +0100

    Added project to PyPi
    https://pypi.org/project/vermin/

---
+++ setup.py
@@ -0,0 +1,60 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+  long_description = f.read()
+
+setup(
+  name="vermin",
+  version="0.2.1",
+
+  description="Concurrently detect the minimum Python versions needed to run code",
+  long_description=long_description,
+
+  url="https://github.com/netromdk/vermin",
+
+  author="Morten Kristensen",
+  author_email="msk@nullpointer.dk",
+
+  license="MIT",
+
+  classifiers=[
+    "Development Status :: 4 - Beta",
+
+    "Intended Audience :: Developers",
+
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+
+    "License :: OSI Approved :: MIT License",
+
+    "Topic :: Utilities",
+    "Topic :: Software Development",
+  ],
+
+  keywords="version detection analysis ast development",
+
+  packages=find_packages(exclude=["tests"]),
+
+  python_requires=">=2.7, >=3",
+
+  entry_points={
+    "console_scripts": [
+      "vermin=vermin:main",
+    ],
+  },
+
+  project_urls={
+    "Bug Reports": "https://github.com/netromdk/vermin/issues",
+    "Source": "https://github.com/netromdk/vermin/",
+  },
+)
--- vermin/__init__.py
+++ vermin/__init__.py
@@ -1,3 +1,4 @@
+from .main import main
 from .detection import detect_min_versions_source, detect_min_versions_path, combine_versions,\
   InvalidVersionException, detect_paths
 from .source_visitor import SourceVisitor
@@ -6,6 +7,7 @@ from .processing import process_paths
 from .rules import MOD_REQS, MOD_MEM_REQS, KWARGS_REQS, STRFTIME_REQS
 
 __all__ = [
+  "main",
   "detect_min_versions_source",
   "detect_min_versions_path",
   "combine_versions",
