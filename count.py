49e9b9b6f6b294062dd028fcbd0c910d48680735
Morten Kristensen <msk@nullpointer.dk>
2018-03-15 21:18:05 +0100

    Fixed count script

--- count.py
+++ count.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 from vermin import MOD_REQS, MOD_MEM_REQS, KWARGS_REQS, STRFTIME_REQS
-(mods, kwargs, dirs) = (len(MOD_REQS), len(KWARGS_REQS), len(STRFTIME_REQS))
-mems = 0
-for v in MOD_MEM_REQS.values():
-  mems += len(v)
+(mods, mems, kwargs, dirs) =\
+  (len(MOD_REQS), len(MOD_MEM_REQS), len(KWARGS_REQS), len(STRFTIME_REQS))
 print("Rules:\n{:>4} modules\n{:>4} members\n{:>4} kwargs\n{:>4} strftime directives\n{:>4} total".
       format(mods, mems, kwargs, dirs, mods + mems + kwargs + dirs))
