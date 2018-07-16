212a3f186cfa82158ea50aba9f6e1d03989ff5c9
raphael <raphael.cohen@blueliv.com>
2018-07-16 15:09:33 +0200

    Code formatting

--- tests/kwargs.py
+++ tests/kwargs.py
@@ -544,7 +544,9 @@ class VerminKwargsTests(VerminTest):
     self.assertOnlyIn((2.3, 3.0), detect("import math\nmath.log(base=None)"))
 
   def test_exit_ok_of_path_mkdir(self):
-    self.assertOnlyIn(3.5, detect("from pathlib import Path\np=Path('foo')\np.mkdir(exist_ok=True)"))
+    self.assertOnlyIn(
+      3.5, detect("from pathlib import Path\np=Path('foo')\np.mkdir(exist_ok=True)"))
 
   def test_strict_of_path_resolve(self):
-    self.assertOnlyIn(3.6, detect("from pathlib import Path\np=Path('foo')\np.resolve(strict=True)"))
+    self.assertOnlyIn(
+      3.6, detect("from pathlib import Path\np=Path('foo')\np.resolve(strict=True)"))
