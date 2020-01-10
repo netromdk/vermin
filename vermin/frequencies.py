13a945973336e1e3ff4a6dae3e3a3fa8f0aaaa96
Morten Kristensen <me@mortens.dev>
2020-01-10 19:07:04 +0100

    Encapsulate frequency entries

--- vermin/frequencies.py
+++ vermin/frequencies.py
@@ -1,26 +1,42 @@
 import json
 from os.path import abspath
 
+class FreqEntry:
+  def __init__(self):
+    self.__count = 0
+
+  def count(self):
+    return self.__count
+
+  def inc(self):
+    self.__count += 1
+
+  def unite(self, entry):
+    self.__count += entry.count()
+
+  def data(self):
+    return (self.count(),)
+
 class Frequencies:
   def __init__(self):
-    self.reset()
-
-  def reset(self):
     self.__members = dict()
+
+  def members(self):
+    return self.__members
 
   def data(self):
     return {
-      "members": self.__members
+      "members": self.__entries_data(self.__members)
     }
 
   def unite(self, freq):
-    other_data = freq.data()
-    for mem in other_data["members"]:
-      count = other_data["members"][mem]
+    other_members = freq.members()
+    for mem in other_members:
+      entry = other_members[mem]
       if mem in self.__members:
-        self.__members[mem] += count
+        self.__members[mem].unite(entry)
       else:
-        self.__members[mem] = count
+        self.__members[mem] = entry
 
   def record_member(self, member):
     self.__inc(self.__members, member)
@@ -31,6 +47,11 @@ class Frequencies:
       json.dump(self.data(), fp, indent=2, sort_keys=True)
 
   def __inc(self, dictionary, value):
-    if value not in dictionary:
-      dictionary[value] = 0
-    dictionary[value] += 1
+    dictionary.setdefault(value, FreqEntry())
+    dictionary[value].inc()
+
+  def __entries_data(self, dictionary):
+    res = {}
+    for key in dictionary:
+      res[key] = dictionary[key].data()
+    return res
