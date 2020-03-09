d349ec38eb18b1fca672cf849b55107b63398dbc
Morten Kristensen <me@mortens.dev>
2020-03-09 19:26:39 +0100

    Improve literal annotation testing
    Related to #40.

--- tests/annotation.py
+++ tests/annotation.py
@@ -1,4 +1,4 @@
-from .testutils import VerminTest, detect, current_version
+from .testutils import VerminTest, detect, current_version, visit
 
 class VerminModuleTests(VerminTest):
   def test_return_annotation(self):
@@ -21,7 +21,18 @@ class VerminModuleTests(VerminTest):
 
   def test_Literal_annotation(self):
     if current_version() >= 3.6:
-      self.assertOnlyIn((3, 8), detect("a: Literal = 5"))
-      self.assertOnlyIn((3, 8), detect("a: Literal[int] = 5"))
-      self.assertOnlyIn((3, 8), detect("def only_four(x: Literal[4]):\n\treturn x"))
-      self.assertOnlyIn((3, 8), detect("def foo(x: typing.Literal[4]):\n\treturn x"))
+      visitor = visit("a: Literal = 5")
+      self.assertTrue(visitor.literal_annotations())
+      self.assertOnlyIn((3, 8), visitor.minimum_versions())
+
+      visitor = visit("a: Literal[int] = 5")
+      self.assertTrue(visitor.literal_annotations())
+      self.assertOnlyIn((3, 8), visitor.minimum_versions())
+
+      visitor = visit("def only_four(x: Literal[4]):\n\treturn x")
+      self.assertTrue(visitor.literal_annotations())
+      self.assertOnlyIn((3, 8), visitor.minimum_versions())
+
+      visitor = visit("def foo(x: typing.Literal[4]):\n\treturn x")
+      self.assertTrue(visitor.literal_annotations())
+      self.assertOnlyIn((3, 8), visitor.minimum_versions())
