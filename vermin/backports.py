74b0b6aa8a316644ee43d2a539f48a1a3e0fbc2c
Morten Kristensen <me@mortens.dev>
2020-08-15 12:10:42 +0200

    Correctly enumerate backport modules for stringification

--- vermin/backports.py
+++ vermin/backports.py
@@ -10,7 +10,7 @@ class Backports:
   @staticmethod
   def str(indent=0):
     res = []
-    longest = len(max({"typing", "configparser", "enum", "faulthandler"}, key=lambda x: len(x)))
+    longest = len(max(Backports.modules(), key=lambda x: len(x)))
     for (mod, link) in BACKPORTS:
       res.append("{}{:{fill}} - {}".format(" " * indent, mod, link, fill=longest))
     return "\n".join(res)
