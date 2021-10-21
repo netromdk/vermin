a8e668a11480d08b449f3a41f12975a7f984fe28
Morten Kristensen <me@mortens.dev>
2021-10-21 18:08:31 +0200

    [actions] Don't suppress test errors for Python 2.7, 3.4, and 3.5

--- misc/actions/test.ps1
+++ misc/actions/test.ps1
@@ -2,33 +2,33 @@ $PYTHON_VERSION = python -c "import sys;v=sys.version_info;print('{}.{}'.format(
 
 if ( $PYTHON_VERSION -eq "2.7" -or $PYTHON_VERSION -eq "3.4" -or $PYTHON_VERSION -eq "3.5" ) {
   make test
-  exit 0
-}
-
-# Set policy that allows the execution of scripts and tests.
-Set-ExecutionPolicy Unrestricted -Force
-
-# Setup virtual env and install deps if virtual env doesn't already exist.
-if (-not (Test-Path -LiteralPath ".venv")) {
-  make install-deps-user setup-venv
-  . .venv\Scripts\activate.ps1
-  make setup-coverage
 }
 else {
-  . .venv\Scripts\activate.ps1
-}
+  # Set policy that allows the execution of scripts and tests.
+  Set-ExecutionPolicy Unrestricted -Force
 
-# Run tests+program and record coverage.
-coverage run --source=vermin,tests runtests.py
-if (!$?) {
-  $Host.SetShouldExit(1) # This is necessary in order to fail the GitHub job.
-  exit 1
-}
-coverage run --append --source=vermin ./vermin.py -v -t="2.7" -t=3 vermin.py vermin
-if (!$?) {
-  $Host.SetShouldExit(1)
-  exit 1
-}
+  # Setup virtual env and install deps if virtual env doesn't already exist.
+  if (-not (Test-Path -LiteralPath ".venv")) {
+    make install-deps-user setup-venv
+    . .venv\Scripts\activate.ps1
+    make setup-coverage
+  }
+  else {
+    . .venv\Scripts\activate.ps1
+  }
 
-# Report coverage. Note that it requires COVERALLS_REPO_TOKEN to be set!
-coveralls
+  # Run tests+program and record coverage.
+  coverage run --source=vermin,tests runtests.py
+  if (!$?) {
+    $Host.SetShouldExit(1) # This is necessary in order to fail the GitHub job.
+    exit 1
+  }
+  coverage run --append --source=vermin ./vermin.py -v -t="2.7" -t=3 vermin.py vermin
+  if (!$?) {
+    $Host.SetShouldExit(1)
+    exit 1
+  }
+
+  # Report coverage. Note that it requires COVERALLS_REPO_TOKEN to be set!
+  coveralls
+}
--- misc/actions/test.sh
+++ misc/actions/test.sh
@@ -5,23 +5,22 @@ PYTHON_VERSION=$(python -c "import sys;v=sys.version_info;print('{}.{}'.format(v
 
 if [[ $PYTHON_VERSION = 2.7 || $PYTHON_VERSION = 3.4 || $PYTHON_VERSION = 3.5 ]]; then
   make test
-  exit 0
-fi
-
-setup() {
-  make install-deps setup-venv
-  source .venv/bin/activate
-  make setup-coverage
-}
-
-# Even if .venv is found, make sure required executables are found, otherwise setup anyway.
-if [[ ! -d .venv ]] || [[ ! -f .venv/bin/coveralls ]]; then
-  setup
 else
-  source .venv/bin/activate
+  setup() {
+    make install-deps setup-venv
+    source .venv/bin/activate
+    make setup-coverage
+  }
+
+  # Even if .venv is found, make sure required executables are found, otherwise setup anyway.
+  if [[ ! -d .venv ]] || [[ ! -f .venv/bin/coveralls ]]; then
+    setup
+  else
+    source .venv/bin/activate
+  fi
+
+  make test-coverage
+
+  # Note that it requires COVERALLS_REPO_TOKEN to be set!
+  coveralls
 fi
-
-make test-coverage
-
-# Note that it requires COVERALLS_REPO_TOKEN to be set!
-coveralls
