e7de7f7315846e763dd62e2560c2982e747cefe8
Morten Kristensen <me@mortens.dev>
2022-12-20 11:24:09 +0100

    [actions] Don't test using EOL Python 3.6

--- misc/actions/test.sh
+++ misc/actions/test.sh
@@ -3,7 +3,11 @@ set -x
 
 PYTHON_VERSION=$(python -c "import sys;v=sys.version_info;print('{}.{}'.format(v[0],v[1]))")
 
-if [[ $PYTHON_VERSION = 2.7 || $PYTHON_VERSION = 3.4 || $PYTHON_VERSION = 3.5 ]]; then
+if [[ $PYTHON_VERSION = 2.7 ||
+      $PYTHON_VERSION = 3.4 ||
+      $PYTHON_VERSION = 3.5 ||
+      $PYTHON_VERSION = 3.6 ]];
+then
   make test
 else
   setup() {
